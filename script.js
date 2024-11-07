90b4c7ef2b047eb89306e56c08eae5f19a8a925a
Sahan Chamara <sahanchamara456@gmail.com>
2024-11-07 13:34:12 +0530

    all countries view

--- script.js
+++ script.js
@@ -1,66 +1,29 @@
-loadItems();
+allConutry();
 
-async function loadItems() {
-    let res = await fetch("https://restcountries.com/v3.1/all");
-    let items = await res.json();
-    let body = "";
-    items.forEach(element => {
-        console.log(element);
-        body += `
-          <div class="col">
-          <div class="card shadow-sm">
-            <img src=${element.flags.png} alt="">
-            <div class="card-body">
-            <p>${element.name.common}</p>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        `;
-    });
-    console.log(body);
-    document.getElementById("row").innerHTML = body;
+
+async function allConutry() {
+  let res = await fetch("https://restcountries.com/v3.1/all");
+  let countries = await res.json();
+  let body = "";
+  countries.forEach(elements => {
+    body += `<div class="col-lg-2 col-md-5 col-sm-12 p-2 ">
+                    <div class="card" style="width: auto;">
+                        <img src=${elements.flags.png} class="card-img-top" style="height: 8rem; width: auto;" alt="...">
+                        <div class="card-body">
+                            <h6 class="card-title">Card title</h6>
+                            <p class="card-text"></p>
+                            <a href="#" class="btn btn-primary p-2">Click</a>
+                        </div>
+                    </div>
+                </div>`
+  });
+
+  document.getElementById("row").innerHTML=body;
+}
+
+function countrySearch(){
+  
 }
 
 
-let countrySearch = document.getElementById("country").value;
 
-async function search() {
-    let res = await fetch("https://restcountries.com/v3.1/all");
-    let items = await res.json();
-    let body = "";
-    items.forEach(element => {
-        console.log(element);
-        if (countrySearch == element.name.common) {
-            body = `
-          <div class="col">
-          <div class="card shadow-sm">
-            <img src=${element.flags.png} alt="">
-            <div class="card-body">
-            <p>${element.name.common}</p>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        `;
-
-        }
-
-    });
-    console.log(body);
-    document.getElementById("row").innerHTML = body;
-}
